7b81731ebf3c70890c4f8818dd3380ed73036560
Harianto <hariantoatwork@gmail.com>
2020-04-02 09:44:45 +0200

    add storageModel

---
+++ src/lib/storageModel.js
@@ -0,0 +1,32 @@
+const IndexedDBPromise = require('./IndexedDBPromise')
+const biscuit = new IndexedDBPromise('ghostDb', 'biscuit')
+const posts = new IndexedDBPromise('ghostDb', 'posts')
+const storageModel = {
+	get lastBuildDate() {
+		return (async () => {
+			try {
+				const { value } = await biscuit.get('lastBuildDate')
+				return value
+			} catch (e) {
+				return undefined
+			}
+		})()
+	},
+	set lastBuildDate(value) {
+		;(async () => {
+			return await biscuit.put({ id: 'lastBuildDate', value })
+		})()
+	},
+	get posts() {
+		return (async () => {
+			return await posts.getAll().catch(() => [])
+		})()
+	},
+	set posts(value) {
+		;(async () => {
+			return await posts.put(value)
+		})()
+	}
+}
+
+module.exports = storageModel
