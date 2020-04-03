10a313322072caec41e985274949e0f0804398a2
Harianto <hariantoatwork@gmail.com>
2020-04-03 02:20:15 +0200

    storage: query

--- src/lib/storageModel.js
+++ src/lib/storageModel.js
@@ -2,6 +2,12 @@ const IndexedDBPromise = require('./IndexedDBPromise')
 const biscuit = new IndexedDBPromise('ghostDb', 'biscuit')
 const posts = new IndexedDBPromise('ghostDb', 'posts')
 const storageModel = {
+	get query() {
+		return window.localStorage.getItem('query') || ''
+	},
+	set query(value) {
+		window.localStorage.setItem('query', value)
+	},
 	get lastBuildDate() {
 		return (async () => {
 			try {
